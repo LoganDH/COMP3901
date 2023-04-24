a3a3b9b05b95724ad39ed58722c95cb130b8297d
Logan Halsall <logandhalsall@outlook.com>
2023-04-23 22:21:40 -0500

    added schools table

--- app/forms.py
+++ app/forms.py
@@ -4,8 +4,16 @@ from wtforms.validators import InputRequired, Email
 
 class UserForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
-    # password = PasswordField('Password', validators=[InputRequired()])
+    first_name = StringField('First Name', validators=[InputRequired()])
+    last_name = StringField('Last Name', validators=[InputRequired()])
     email = StringField('Email', validators=[InputRequired(), Email()])
+    # password = PasswordField('Password', validators=[InputRequired()])
 
 class EventForm(FlaskForm):
     event = StringField('Event', validators=[InputRequired()])
+
+class SchoolForm(FlaskForm):
+    name = StringField('School Name', validators=[InputRequired()])
+    address = StringField('Address', validators=[InputRequired()])
+    phone = StringField('Phone', validators=[InputRequired()])
+    email = StringField('E-Mail', validators=[InputRequired(), Email()])
--- app/models.py
+++ app/models.py
@@ -7,26 +7,47 @@ class User(db.Model):
     # __tablename__ = "user_profiles"
 
     id = db.Column(db.Integer, primary_key=True)
+    school_id = db.Column(db.ForeignKey("schools.id"))
     username = db.Column(db.String(80), unique=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
     email = db.Column(db.String(120), unique=True)
-    #password = db.Column(db.String(255))
+    # password = db.Column(db.String(255))
 
-    def __init__(self, username, email):
+    def __init__(self, username, first_name, last_name, email):
         self.username = username
+        self.first_name = first_name
+        self.last_name = last_name
         self.email = email
-        #self.password = generate_password_hash(password)
+        # self.password = generate_password_hash(password)
 
     def __repr__(self):
         return '<User %r>' % self.username
+    
+class Schools(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True)
+    address = db.Column(db.String(255), unique=True)
+    phone = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, name, address, phone, email):
+        self.name = name
+        self.address = address
+        self.phone = phone
+        self.email = email
+    
+    def __repr__(self):
+        return '<School %r>' % self.name
 
 class Events(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    #case_id = db.Column()
+    # case_id = db.Column()
     date = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
     entry = db.Column(db.String(500))
-    #location = db.Column()
-    #submitted_by = db.Column()
-    #status = db.Column()
+    # location = db.Column()
+    # submitted_by = db.Column()
+    # status = db.Column()
 
     def __init__(self, entry):
         self.entry = entry
